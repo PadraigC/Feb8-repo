8b762feaf2f6415e9642ea521931173b2ea35f4a
Padraig Cunningham <padraig.cunningham@ucd.ie>
2013-02-08 09:42:42 +0000

    C1

---
+++ GitHub-Feb8/src/GitHub-Feb8.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : GitHub-Feb8.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!GitHub Feb 28!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
